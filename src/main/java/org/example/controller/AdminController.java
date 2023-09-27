fddeb01fb6dc94f866d5a1396c923d06b276332b
TrioL1 <losma2621@gmail.com>
2023-09-27 16:21:45 +0900

    로그인 구현 완료 - 로그인 시 계정명 띄우기 완료(이름 띄우는 것도 구현완료)

--- src/main/java/org/example/controller/AdminController.java
+++ src/main/java/org/example/controller/AdminController.java
@@ -1,5 +1,8 @@
 package org.example.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.entity.Member;
+import org.example.repository.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
 
+
+    private final MemberRepository memberRepository;
+
     @GetMapping(value= "/admin")
-    public String adminroot() {
-        //model.addAttribute("user",principal.getName());
-        return "/admin/root";
+    public String adminroot(Principal principal, Model model) {
+        Member member = memberRepository.findByUserId(principal.getName());
+        String name = (member != null) ? member.getUserName() : "Unknown"; // Member 테이블에서 이름 가져오기
+
+        model.addAttribute("name", name);
+        return "admin/root";
     }
 
 }
