d8f8a3316f267eae8dda0dcfd987a63fbc2f2e72
TrioL1 <losma2621@gmail.com>
2023-09-27 16:12:12 +0900

    로그인 구현 완료 - 로그인 시 계정명 띄우기 완료(이름 띄우는 것 미구현)

--- src/main/java/org/example/controller/AdminController.java
+++ src/main/java/org/example/controller/AdminController.java
@@ -1,13 +1,17 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class AdminController {
 
     @GetMapping(value= "/admin")
     public String adminroot() {
+        //model.addAttribute("user",principal.getName());
         return "/admin/root";
     }
 
