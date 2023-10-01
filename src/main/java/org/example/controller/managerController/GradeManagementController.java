19aff62ba24e89c9dbb58771334be621b563aa27
dlagnlwo <gnlwo7606@naver.com>
2023-10-01 15:00:10 +0900

    2023-10-01

--- src/main/java/org/example/controller/managerController/GradeManagementController.java
+++ src/main/java/org/example/controller/managerController/GradeManagementController.java
@@ -8,14 +8,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/manager")
 public class GradeManagementController {
 
+    /*
+    전체관리성적
+     */
     @GetMapping("/grade")
     public String grade(){
         return "/manager/gradeManagement";
     }
 
+    /*
+    게시글 관리(담당용)
+     */
     @GetMapping("/postWrite")
     public String postWrite(){
         return "/manager/postWrite";
     }
 
+    /*
+    게시글 정보관리
+     */
+    @GetMapping("/postInfo")
+    public String postInfo(){
+        return "/manager/postInfo";
+    }
 }
