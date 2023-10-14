ed117a583a6fac5bc0a96860fc0cf5d3608438c4
Yujin051 <sofi04080@gmail.com>
2023-10-14 14:59:33 +0900

    소소한 수정

--- src/main/java/org/example/controller/admin/YoutubeAuthController.java
+++ src/main/java/org/example/controller/admin/YoutubeAuthController.java
@@ -6,18 +6,21 @@ import org.example.service.admin.YoutubeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-public class AuthController {
+public class YoutubeAuthController {
 
     private final YoutubeService youtubeService;
 
-    // 버튼 눌렀을 때 인증 화면으로 전송하는 컨트롤러
+    // 버튼 눌렀을 때 인증 토큰 얻어오는 컨트롤러
     @GetMapping("/youtubeAPIAuth")
     public String youtubeAuth() {
         String redirectUrl = "https://accounts.google.com/o/oauth2/v2/auth?" +
-                "scope=https://www.googleapis.com/auth/youtube.upload&" +
+                "scope=https://www.googleapis.com/auth/youtube " +
+                "https://www.googleapis.com/auth/youtube.upload&" +
                 "redirect_uri=http://localhost/youtubeAuthToken&" +
                 "response_type=code&access_type=offline&" +
                 "client_id=278647557012-a4i2nt3vtvkaimsk4g2josu21btucaf1.apps.googleusercontent.com";
@@ -25,11 +28,19 @@ public class AuthController {
         return "redirect:" + redirectUrl;
     }
 
+    // 인증 코드로 액세스 토큰 얻는 컨트롤러
     @GetMapping("/youtubeAuthToken")
-    public String youtubeToken(@RequestParam(name = "code") String code) {
+    public String youtubeToken(@RequestParam(name = "code") String code, Principal principal) {
         log.info("코드 : {}", code);
-        youtubeService.getAccessToken(code);
+        String accessToken = youtubeService.getCredential(code, principal);
+        log.info("액세스토큰 : {}", accessToken);
 
         return "/admin/youTube_registration";
+    }
+
+    // 동영상 업로드 컨트롤러
+    @PostMapping("/youtubeUpload")
+    public void youtubeUpload() {
+
     }
 }
--- src/main/java/org/example/service/admin/YoutubeService.java
+++ src/main/java/org/example/service/admin/YoutubeService.java
@@ -1,19 +1,21 @@
 package org.example.service.admin;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.MemoryDataStoreFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.InputStreamReader;
-import java.util.Collections;
+import java.security.Principal;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -21,40 +23,46 @@ import java.util.List;
 public class YoutubeService {
     private static final String CLIENT_SECRETS_FILE = "/static/OAuthClientSecretToken/client_secrets.json";
     private static final String REDIRECT_URI = "http://localhost/youtubeAuthToken"; // 등록한 리디렉션 URI
-    private static final List<String> SCOPES = Collections.singletonList("https://www.googleapis.com/auth/youtube.upload"); // 필요한 스코프
+    private static final List<String> SCOPES = Arrays.asList("https://www.googleapis.com/auth/youtube",
+            "https://www.googleapis.com/auth/youtube.upload"); // 필요한 스코프, 유튜브 계정과 업로드 권한
+    private static final String FILE_DIRECTORY = "C:\\app\\java\\lms-system-team-project\\src\\main\\resources\\static\\OAuthClientSecretToken\\AccessToken";
 
+    // 인증 코드 기반 액세스 토큰 얻는 서비스
+    public String getCredential(String authCode, Principal principal){
 
-    public String getAccessToken (String authorizationCode){
-
-        log.info("code : {}", authorizationCode);
+        log.info("code : {}", authCode);
+        File dataDir = new File(FILE_DIRECTORY);
 
         try {
             HttpTransport httpTransport = new NetHttpTransport();
-            GsonFactory gsonFactory = GsonFactory.getDefaultInstance();
+            GsonFactory gsonFactory = new GsonFactory();
+            FileDataStoreFactory fileDataStoreFactory = new FileDataStoreFactory(dataDir);
+
 
             GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(gsonFactory, new InputStreamReader(
                     YoutubeService.class.getResourceAsStream(CLIENT_SECRETS_FILE)
             ));
 
             log.info("clientSecrets : {}", clientSecrets);
+            log.info("dataDir : {}", dataDir);
 
             GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                     httpTransport, gsonFactory, clientSecrets, SCOPES)
-                    .setDataStoreFactory(new MemoryDataStoreFactory())
+                    .setDataStoreFactory(fileDataStoreFactory)
                     .setAccessType("offline")
                     .build();
 
-            log.info("flow : {}", flow.getAccessType());
-
-            TokenResponse tokenResponse = flow.newTokenRequest(authorizationCode)
+            GoogleTokenResponse tokenResponse = flow
+                    .newTokenRequest(authCode)
                     .setRedirectUri(REDIRECT_URI)
                     .execute();
 
+            log.info("flow : {}", flow);
             log.info("tokenResponse : {}", tokenResponse);
 
-            Credential credential = flow.createAndStoreCredential(tokenResponse, null);
+            Credential credential = flow.createAndStoreCredential(tokenResponse, principal.getName());
 
-            log.info(credential.getAccessToken());
+//            log.info(credential.getAccessToken());
 
             return credential.getAccessToken();
         } catch (GoogleJsonResponseException e) {
@@ -62,12 +70,14 @@ public class YoutubeService {
             e.printStackTrace();
             e.getDetails();
             e.getMessage();
-            return null;
+            return "구글 API 에러 발생";
         } catch (Exception e) {
             // 예외 처리
             e.printStackTrace();
-            return null;
+            return "에러 발생";
         }
     }
+
+
 
 }
